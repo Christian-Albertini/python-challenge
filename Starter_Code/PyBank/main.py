1efc49522f8dbe1b6f846182899a394ddeb2b109
Christian Albertini <cja12classic@hotmail.com>
2024-04-17 18:23:58 -0400

    Updates to Python Challenge

--- Starter_Code/PyBank/main.py
+++ Starter_Code/PyBank/main.py
@@ -1,40 +1,55 @@
+# Import Modules
 import os
 import csv
 
-
+# Path to collect data from the Resources folder
 psybank_csv = os.path.join("Resources", "budget_data.csv")
 file_path = "Analysis/analysis.txt"
 files_path = "Resources/budget_data.csv"
 
-
+# Set values and lists
 net_total = 0
 close = 0
 large_inc = []
 large_dec = []
 prices = []
 
+# Function to find largest increase
 def find_inc(lst):
+    # Create values
     max_increase = 0
     months_num = 0
+    # Loop through list
     for i in range(1, len(lst)):
+        # Set increase to be the change from i-1 to i
         increase = int(lst[i])-int(lst[i-1])
+        # Check to see if increase is greater than max increase
         if increase > max_increase:
+            # If so, max_increase becomes increase and the value for the month it happens in is set as i
             max_increase = increase
             months_num = i
+    # Return the max increase and the number cooresponding with the month it happens in
     return [months_num, max_increase]
     
 
-
+# Function to find largest decrease
 def find_dec(lst):
+    # Create values
     max_decrease = 0
     month_num = 0
+    # Loop through list
     for i in range(1, len(lst)):
+        # Set decrease to be the difference between i-1 and i
         decrease = int(lst[i-1]) - int(lst[i])
+        # Check to see if decrease is greater than max decrease
         if decrease > max_decrease:
+            # If so, max decrease becomes decrease and set month to be the i
             max_decrease = decrease
             month_num = i
+    # Return max decrease and number of month it happens in
     return [month_num, max_decrease]
 
+# Function I found online to let me find the value in the csv file that matches the month of the greatest increase and decrease without having to change my functions
 def retrieve_csv_element(csv_file_path, row_index, column_index):
     try:
         # Read the CSV file
@@ -59,17 +74,18 @@ def retrieve_csv_element(csv_file_path, row_index, column_index):
         print(f"An error occurred: {e}")
 
 
-
+# Read in CSV file
 with open(psybank_csv) as csvfile:
-
+# Split the data on commas
     csvreader = csv.reader(csvfile, delimiter=",")
-
+# Get header
     csv_header = next(csvreader)
-
+# Loop through the data in the csv file
     for rows in csvreader:
+        # Increase net total and add profits/losses to prices list
         net_total += int(rows[1])
         prices.append(rows[1])
-
+    # Set values to equal the correct numbers
     open_price = prices[0]
     close = prices[-1]
     months = len(prices)
@@ -80,13 +96,13 @@ with open(psybank_csv) as csvfile:
     large_dec_val = large_dec[1]
     dec_month_num = large_dec[0]
     column_index = 0
-
+    # Get month of largest increase or decrease using function
     inc_month = retrieve_csv_element(files_path, (inc_month_num+1), column_index)
     dec_month = retrieve_csv_element(files_path, (dec_month_num+1), column_index)
-
+    # Make change have only 2 numbers after the decimal
     change = round(((int(close) - int(open_price))/(int(months)-1)), 2)
 
-
+# Create and write text file in analysis folder
 with open(file_path, 'w') as file:
     file.write("Financial Analysis\n")
     file.write("\n")
@@ -102,6 +118,7 @@ with open(file_path, 'w') as file:
     file.write("\n")
     file.write("Greatest Decrease in Profits: " + dec_month + " ($-" + str(large_dec_val) + ")\n")
 
+# Print same info from text file in terminal
 print("Financial Analysis")
 print("")
 print("--------------------------------------------------")
--- Starter_Code/PyPoll/main.py
+++ Starter_Code/PyPoll/main.py
@@ -1,30 +1,40 @@
+# Import modules
 import os
 import csv
 
-
+# Path to collect data from the Resource folder and create new file in analysis folder
 pypoll_csv = os.path.join("Resources", "election_data.csv")
 file_path = "analysis/analysis.txt"
 
+# Set values and dictionary
 total_votes = 0
 candidates = {}
 
+# Read in CSV file
 with open(pypoll_csv) as csvfile:
+    # Split the data on commas
     csvreader = csv.reader(csvfile, delimiter = ",")
-
+    # Get header
     csv_header = next(csvreader)
-
+    # Loop through the file
     for rows in csvreader:
+        # Check to see if value in 3rd column of rows is in candidates dictionary
         if rows[2] not in candidates:
+            # If not, create new row in candidate that has the name and sets the count to 0
             candidates[rows[2]] = 0
+        # Adds one to the vote count for the candidate in the row
         candidates[rows[2]] = candidates[rows[2]] + 1
+        # Add one to total votes
         total_votes += 1
     
-    final_list = [{"candidate" : rows, "count" : candidates[rows]} for rows in candidates]
+    # Create a value that sees who won by finding the max value in the candidates list
     winner = max(candidates, key=candidates.get)
+    # Creates a list that holds the keys from the candidates dictionary, which are the names of the candidates
     names = list(candidates.keys())
+    # Creates a list that holds the values from the candidates dictionary, which is the vote counts
     votes = list(candidates.values())
     
-    
+# Create new text file in analysis and write in results
 with open(file_path, 'w') as file:
     file.write("Election Results\n")
     file.write("\n")
@@ -46,6 +56,7 @@ with open(file_path, 'w') as file:
     file.write("\n")
     file.write("--------------------------------------------------\n")
 
+# Print same results from text file in the terminal
 print("Election Results")
 print("")
 print("--------------------------------------------------")
